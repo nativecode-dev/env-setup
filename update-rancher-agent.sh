dcf21d4bd5fb64c9ca13bb65569fcba4b6337004
Mike Pham <mike.pham@nativecode.com>
2018-02-27 14:22:58 +0000

    fix: adding rancher agent update script and wordpress permissions fix

--- update-rancher-agent.sh
+++ update-rancher-agent.sh
@@ -6,23 +6,34 @@ CONTAINER_PATH=$CONTAINER_DIR/$CONTAINER_ID
 CONTAINER_CONFIG=$CONTAINER_PATH/config.v2.json
 
 if [ $1 == "update" ]; then
-  if [ -f $CONTAINER_CONFIG ]; then
+
+    if [ -f $CONTAINER_CONFIG ]; then
+
     cp $CONTAINER_CONFIG $CONTAINER_CONFIG.bak
+
     systemctl stop docker
+
     sed -i 's/CATTLE_AGENT_IP/CATTLE_AGENT_IP='"$COREOS_PRIVATE_IPV4"'/g' $CONTAINER_CONFIG
+
     systemctl start docker
+
   fi
 
-  exit 0
 fi
 
 if [ $1 == "restore" ]; then
+
   if [ -f $CONTAINER_CONFIG ]; then
+
     cp $CONTAINER_CONFIG $CONTAINER_CONFIG.bak
+
     systemctl stop docker
+
     sed -i 's/CATTLE_AGENT_IP='"$COREOS_PRIVATE_IPV4"'/CATTLE_AGENT_IP/g' $CONTAINER_CONFIG
+
     systemctl start docker
+
   fi
 
-  exit 0
 fi
+
