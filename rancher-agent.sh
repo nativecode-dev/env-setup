95861d6f5cd7e7fd9211281320d99aaf1a46610c
Mike Pham <mike.pham@nativecode.com>
2018-03-01 01:54:30 +0000

    fixing rancher-agent

--- rancher-agent.sh
+++ rancher-agent.sh
@@ -5,8 +5,12 @@ CONTAINER_ID=`docker inspect --format="{{.Id}}" rancher-agent`
 CONTAINER_PATH=$CONTAINER_DIR/$CONTAINER_ID
 CONTAINER_CONFIG=$CONTAINER_PATH/config.v2.json
 
-if [ $1 == "update" ]; then
+echo "Use rancher-update [update|restore]"
+echo "$1"
+
+if [ "$1" == "update" ]; then
   if [ -f $CONTAINER_CONFIG ]; then
+    echo "Updating container $CONTAINER_ID..."
     cp $CONTAINER_CONFIG $CONTAINER_CONFIG.bak
     systemctl stop docker
     sed -i 's/CATTLE_AGENT_IP/CATTLE_AGENT_IP='"$COREOS_PRIVATE_IPV4"'/g' $CONTAINER_CONFIG
@@ -16,8 +20,9 @@ if [ $1 == "update" ]; then
   exit 0
 fi
 
-if [ $1 == "restore" ]; then
+if [ "$1" == "restore" ]; then
   if [ -f $CONTAINER_CONFIG ]; then
+    echo "Restoring $CONTAINER_CONFIG..."
     cp $CONTAINER_CONFIG $CONTAINER_CONFIG.bak
     systemctl stop docker
     sed -i 's/CATTLE_AGENT_IP='"$COREOS_PRIVATE_IPV4"'/CATTLE_AGENT_IP/g' $CONTAINER_CONFIG
@@ -26,3 +31,4 @@ if [ $1 == "restore" ]; then
 
   exit 0
 fi
+
