4c0089a03cdecccc280bf7ea5fd33de3570a1c5b
Mike Pham <support@nativecode.com>
2018-03-12 13:26:25 -0400

    merge

--- containers/pihole/scripts/updater.sh
+++ containers/pihole/scripts/updater.sh
@@ -12,6 +12,9 @@ fi
 echo "Creating lock file"
 touch ${PIHOLE_YOUTUBE}/update.lock
 
+TIMESTAMP_FILE=${PIHOLE_YOUTUBE_LIST_NAME}.`date +%s%N | cut -b1-13`
+TIMESTAMP_FILE_LIST=/tmp/${TIMESTAMP_FILE}
+
 # Add youtube ads list to default list.
 if [ ! -f ${PIHOLE_YOUTUBE_LIST} ]; then
   echo "##Local YouTube Ad List" >> ${PIHOLE}/adlists.list
@@ -24,8 +27,15 @@ if [ ! -f /var/www/html/${PIHOLE_YOUTUBE_LIST_NAME} ]; then
   ln -s ${PIHOLE_YOUTUBE_LIST} /var/www/html/${PIHOLE_YOUTUBE_LIST_NAME}
 fi
 
-TIMESTAMP_FILE=${PIHOLE_YOUTUBE_LIST_NAME}.`date +%s%N | cut -b1-13`
-LIST_TIMESTAMP=/tmp/${TIMESTAMP_FILE}
+# Create a backup before we do anything else.
+if [ "$1" = "backup" ]; then
+  echo "Creating backup file archive from $PIHOLE_YOUTUBE_LIST..."
+  mkdir ${PIHOLE_YOUTUBE_BACKUPS} -p
+  cd ${PIHOLE}
+  # Stupid tar, have to be in or below the directory we want, can't be a sibling.
+  tar -czvf ${PIHOLE_YOUTUBE_BACKUPS}/${TIMESTAMP_FILE}.tgz ${PIHOLE_YOUTUBE_LIST_NAME} > /dev/null
+  cd ${PIHOLE_YOUTUBE}
+fi
 
 # Copy the current list to updates for inclusion.
 echo "Creating updates file..."
@@ -36,7 +46,7 @@ cat ${PIHOLE_YOUTUBE_LIST} >> ${PIHOLE_YOUTUBE_LIST_UPDATE}
 grep r*.googlevideo.com ${PIHOLE_LOGS} | awk '{print $8}' | grep -v '^googlevideo.com\|redirector' >> ${PIHOLE_YOUTUBE_LIST_UPDATE}
 
 # Re-select everything and only return a unique list.
-cat ${PIHOLE_YOUTUBE_LIST} > ${LIST_TIMESTAMP}
+cat ${PIHOLE_YOUTUBE_LIST} > ${TIMESTAMP_FILE_LIST}
 cat ${PIHOLE_YOUTUBE_LIST_UPDATE} | sort -n | uniq > ${PIHOLE_YOUTUBE_LIST}
 cat ${PIHOLE_YOUTUBE_LIST} | wc -l > ${PIHOLE_YOUTUBE_LIST}.count
 
@@ -47,16 +57,9 @@ fi
 
 # Shrink timestamp file and remove zero-byte files.
 find /tmp/${PIHOLE_YOUTUBE_LIST_NAME}.* -size  0 -print0 | xargs -0 rm -f
-if [ "$1" = "backup" ]; then
-  if [ -f ${LIST_TIMESTAMP} ]; then
-    echo "Creating backup file archive from $LIST_TIMESTAMP..."
-    mkdir ${PIHOLE_YOUTUBE_BACKUPS} -p
-    tar -czvf ${TIMESTAMP_FILE}.tar.gz ${LIST_TIMESTAMP}
-  fi
-fi
 
-if [ -f ${LIST_TIMESTAMP} ]; then
-  rm ${LIST_TIMESTAMP}
+if [ -f ${TIMESTAMP_FILE_LIST} ]; then
+  rm ${TIMESTAMP_FILE_LIST}
 fi
 
 # Flush pi-hole logs.
