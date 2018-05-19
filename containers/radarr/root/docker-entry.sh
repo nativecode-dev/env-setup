ac2cf6d7a78287cacccce252bc21010d727942f1
Mike Pham <contact@nativecode.com>
2018-05-18 22:08:18 -0400

    config

--- containers/radarr/root/docker-entry.sh
+++ containers/radarr/root/docker-entry.sh
@@ -1,17 +1,18 @@
 #! /bin/sh -e
 
+echo "docker-entry.sh"
+echo ""
 echo "SERVER=$SERVER"
 echo "SHARE=$SHARE"
 echo "NFSVER=$NFSVER"
 echo "OPTIONS=$OPTIONS"
 echo "MOUNT=$MOUNT"
-
+echo ""
 echo "Making directory $MOUNT"
 mkdir -p "$MOUNT"
-
+echo ""
 echo "Starting RPC Bind"
 rpc.statd & rpcbind -f &
-
 echo "Mounting $SERVER:$SHARE to $MOUNT"
 mount.nfs -o "nfsvers=$NFSVER,$OPTIONS" "$SERVER:$SHARE" "$MOUNT"
 mount | grep nfs
