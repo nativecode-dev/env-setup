30f1b7c9c508b5eca9f79d99a46118b484c94074
Mike Pham <contact@nativecode.com>
2018-06-06 12:27:52 -0400

    adding type var

--- containers/mntnfs/root/entry.sh
+++ containers/mntnfs/root/entry.sh
@@ -4,15 +4,16 @@ echo "SERVER=$SERVER"
 echo "SHARE=$SHARE"
 echo "MOUNT_OPTIONS=$MOUNT_OPTIONS"
 echo "MOUNT_POINT=$MOUNT_POINT"
+echo "MOUNT_TYPE=$MOUNT_TYPE"
 echo ""
-echo "Will mount $SERVER:$SHARE to $MOUNT_POINT using $MOUNT_OPTIONS"
+echo "Will mount $SERVER:$SHARE as $MOUNT_TYPE to $MOUNT_POINT using $MOUNT_OPTIONS"
 
 echo "Making mount directory $MOUNT_POINT..."
 mkdir -p "$MOUNT_POINT"
 
 rpc.statd &
 rpcbind -f &
-mount.nfs -o "$MOUNT_OPTIONS" "$SERVER:$SHARE" "$MOUNT_POINT"
+mount -t "$MOUNT_TYPE" -o "$MOUNT_OPTIONS" "$SERVER:$SHARE" "$MOUNT_POINT"
 
 mount | grep nfs
 
