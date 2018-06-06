72a8b2d080bc81672e592f1b7f72e71eac77067c
Mike Pham <contact@nativecode.com>
2018-06-06 12:03:19 -0400

    update default options

--- containers/mntnfs/root/entry.sh
+++ containers/mntnfs/root/entry.sh
@@ -5,7 +5,7 @@ echo "SHARE=$SHARE"
 echo "MOUNT_OPTIONS=$MOUNT_OPTIONS"
 echo "MOUNT_POINT=$MOUNT_POINT"
 echo ""
-echo "$SERVER:$SHARE"
+echo "Will mount $SERVER:$SHARE to $MOUNT_POINT using $MOUNT_OPTIONS"
 
 echo "Making mount directory $MOUNT_POINT..."
 mkdir -p "$MOUNT_POINT"
